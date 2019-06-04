52e9816cf8f1b44e48d893e48bb1879773c14459
christinesn <51234471+christinesn@users.noreply.github.com>
2019-06-04 14:25:33 -0400

    Add tab switching between timer and stopwatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import {Stopwatch} from './Stopwatch';
+import {Timer} from './Timer';
+import {Tabs, Tab} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <Stopwatch />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      key: 'timer'
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Tabs
+          defaultActiveKey="timer"
+          id="switcher"
+          transition={false}
+          onSelect={key => this.setState({ key })}
+        >
+          <Tab eventKey="timer" title="Timer">
+            { this.state.key === 'timer' && (<Timer />) }
+          </Tab>
+          <Tab eventKey="stopwatch" title="Stopwatch">
+            { this.state.key === 'stopwatch' && (<Stopwatch />) }
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme'
+import {Timer} from './Timer'
+import {Stopwatch} from './Stopwatch'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe('Timer and stopwatch rendering', () => {
+    describe('When the timer tab is selected', () => {
+      it("Doesn't render the stopwatch component at all", () => {
+        const wrapper = shallow(<App />)
+
+        wrapper.setState({ key: 'timer' })
+
+        expect(wrapper.exists(Timer)).toEqual(true)
+        expect(wrapper.exists(Stopwatch)).toEqual(false)
+      })
+    })
+
+    describe('When the stopwatch tab is selected', () => {
+      it("Doesn't render the timer component at all", () => {
+        const wrapper = shallow(<App />)
+
+        wrapper.setState({ key: 'stopwatch' })
+
+        expect(wrapper.exists(Timer)).toEqual(false)
+        expect(wrapper.exists(Stopwatch)).toEqual(true)
+      })
+    })
+  })
+})
